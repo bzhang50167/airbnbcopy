15ab339e2ea80f27add4e1b8e3540a0b3307714e
Bao <bao4ltyfe@gmail.com>
2023-03-16 10:35:16 -0700

    user table and seed

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model, Validator } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -21,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       validate:{
         len: [4,30],
         isNotEmail(value) {
-          if (validator.isEmail(value)) {
+          if (Validator.isEmail(value)) {
             throw new Error("Cannot be an email.");
           }
         }
