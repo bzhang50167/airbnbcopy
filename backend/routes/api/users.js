1f20949ec6ea6f1cb038bbc03c06a8e40ccccd3a
Bao <bao4ltyfe@gmail.com>
2023-03-16 13:59:56 -0700

    some work was done

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -48,26 +48,6 @@ router.post(
     }
 );
 
-router.post(
-    '',
-    async (req, res) => {
-        const { email, password, username } = req.body;
-        const hashedPassword = bcrypt.hashSync(password);
-        const user = await User.create({ email, username, hashedPassword });
-
-        const safeUser = {
-            id: user.id,
-            email: user.email,
-            username: user.username,
-        };
-
-        await setTokenCookie(res, safeUser);
-
-        return res.json({
-            user: safeUser
-        });
-    }
-);
 
 
 
