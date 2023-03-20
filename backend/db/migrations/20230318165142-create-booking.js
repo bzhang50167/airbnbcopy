91055b471884a43be6f300dc015296bc52187d30
Bao <bao4ltyfe@gmail.com>
2023-03-20 13:25:13 -0700

    options

--- backend/db/migrations/20230318165142-create-booking.js
+++ backend/db/migrations/20230318165142-create-booking.js
@@ -46,6 +46,7 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bookings');
+    options.tableName = 'Bookings'
+    await queryInterface.dropTable(options);
   }
 };
