856504e525dbc04e5103b1ab1dcba05f7b39e2d2
Bao <bao4ltyfe@gmail.com>
2023-06-07 14:34:38 -0700

    seeders

--- backend/db/seeders/20230318183612-spotImg.js
+++ backend/db/seeders/20230318183612-spotImg.js
@@ -6,31 +6,56 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-options.tableName = 'SpotImages'
+options.tableName = 'SpotImages';
+
+const generateSeedData = () => {
+  const seedData = [];
+
+  for (let i = 1; i <= 100; i++) {
+    seedData.push({
+      spotId: i,
+      url: `https://generatorfun.com/code/uploads/Random-House-image-${i}.jpg`,
+      preview: true
+    });
+  }
+  for (let i = 1; 1 <= 100; i++){
+    seedData.push({
+      spotId: i,
+      url: `https://generatorfun.com/code/uploads/Random-House-image-${i+1}.jpg`,
+      preview: false
+    })
+  }
+  for (let i = 1; 1 <= 100; i++){
+    seedData.push({
+      spotId: i,
+      url: `https://generatorfun.com/code/uploads/Random-House-image-${i+2}.jpg`,
+      preview: false
+    })
+  }
+  for (let i = 1; 1 <= 100; i++){
+    seedData.push({
+      spotId: i,
+      url: `https://generatorfun.com/code/uploads/Random-House-image-${i+3}.jpg`,
+      preview: false
+    })
+  }
+  for (let i = 1; 1 <= 100; i++){
+    seedData.push({
+      spotId: i,
+      url: `https://generatorfun.com/code/uploads/Random-House-image-${i+4}.jpg`,
+      preview: false
+    })
+  }
+
+  return seedData;
+};
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-
-   await queryInterface.bulkInsert(options,[
-    {
-      spotId: 1,
-      url: 'https://a0.muscache.com/im/pictures/7123986c-4f7d-4945-8e5c-eb6bbabb6456.jpg?im_w=960',
-      preview: true
-    },
-    {
-      spotId: 2,
-      url: 'https://a0.muscache.com/im/pictures/7ef420b6-4c3e-463f-825f-3f59e252d369.jpg?im_w=960',
-      preview: true
-    },
-    {
-      spotId: 3,
-      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-51411041/original/d1f938ec-f517-4e5d-a84e-0cc51e48357a.jpeg?im_w=960',
-      preview: true
-    },
-   ])
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(options, generateSeedData());
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     const Op = Sequelize.Op;
     await queryInterface.bulkDelete(options, null, {});
   }
